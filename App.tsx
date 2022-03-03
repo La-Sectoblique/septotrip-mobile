fb3e021981624a385ebe68eabff593712efdbece
ladwein <samuel.ladwein@etu.unistra.fr>
2022-03-03 09:55:46 +0100

    initial commit

--- App.tsx
+++ App.tsx
@@ -1,20 +1,113 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Dimensions, TextInput } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import MapView, { EventActionType, LatLng, Marker, Point, Polyline } from 'react-native-maps'
+import React, { useState } from 'react';
+
+interface ComponentInMap {
+  name?: string;
+  coordinate: LatLng; 
+  position?: Point; 
+}
+interface Route extends ComponentInMap{
+  coordinateEnd: LatLng
+}
+interface MapEvent extends ComponentInMap{
+  action: EventActionType; 
+  id?: string | undefined;
 }
 
 const styles = StyleSheet.create({
-  container: {
+  page: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF'
   },
+  container: {
+    height: 300,
+    width: Dimensions.get("window").width,
+    backgroundColor: 'tomato'
+  },
+  map: {
+    flex: 1
+  },
+  input: {
+    borderWidth: 2,
+    width: Dimensions.get('window').width,
+    paddingVertical: 2,
+    paddingLeft: 10,
+    marginVertical: 10 
+  }
 });
+
+
+
+
+export default function App() {
+  const [markers, setMarkers] = useState<ComponentInMap[]>([])
+  const [routes, setRoutes] = useState<Route[]>([])
+  const [formName, setFormName] = useState<string>('')
+  const [activeComponent, setActiveComponent] = useState<ComponentInMap | Route | null>(null)
+
+  const handleMapPressEvent = (event: MapEvent): void => {
+    if(formName.length == 0)
+      return
+
+    const newMarker = {name:formName, coordinate: event.coordinate, point: event.position}
+    setMarkers(((old: any) => [...old, newMarker]))
+    setFormName('')
+
+    if(markers.length < 1)
+      return
+    const departure = markers[markers.length - 1]
+    const destination = newMarker
+    setRoutes((old: any) => [...old, {name:`${departure.name} - ${destination.name}`, coordinate: departure.coordinate, coordinateEnd: destination.coordinate}])
+  }
+
+  const handlePressEvent = (marker: ComponentInMap): void => {
+    setActiveComponent(marker)
+  }
+
+  return (
+       <SafeAreaView style={styles.page}>
+         {
+            activeComponent == null ? <></>:
+            <>
+              <Text>{activeComponent.name}</Text>
+              <Text>{activeComponent.coordinate.latitude} : {activeComponent.coordinate.longitude}</Text>
+            </>
+          }
+        <View style={styles.container}>
+          
+          <MapView style={styles.map} rotateEnabled={false} onPress={(e) => {handleMapPressEvent(e.nativeEvent)}}>
+            {
+              markers.length == 0 ? <></>:
+              markers.map((marker, i) => <Marker key={i} coordinate={marker.coordinate} anchor={marker.position} onPress={(event) => handlePressEvent(marker)}/>)
+            }
+            {
+              routes.length == 0? <></> :
+              routes.map((route,i) => {
+                return <Polyline
+                  key={i}
+                  coordinates={[
+                    route.coordinate,
+                    route.coordinateEnd
+                  ]}
+                  strokeColor= {i%2? "red": "blue"}
+                  strokeWidth={6}
+                  tappable={true}
+                  onPress={(e) => {handlePressEvent(route)}}
+              />
+              })
+
+            }
+            
+          </MapView>
+        </View>
+        <TextInput style={styles.input} placeholder='nom du marker' value={formName} onChangeText={setFormName}/>
+        <StatusBar style="auto" />
+      </SafeAreaView>
+  );
+}
