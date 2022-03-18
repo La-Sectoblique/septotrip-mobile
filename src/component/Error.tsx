ac3a3ca32e282ca5ab02046920744799e02bd1d3
ladwein <samuel.ladwein@etu.unistra.fr>
2022-03-18 13:43:46 +0100

    feat(Register): Add Register button and error message

---
+++ src/component/Error.tsx
@@ -0,0 +1,23 @@
+
+import React from 'react'
+import { StyleSheet, Text} from 'react-native';
+
+
+interface ErrorProps {
+    error: string, 
+}
+
+export const Error = (props: ErrorProps) => {
+
+    if(props.error.length > 0)
+        return <Text style={style.error} >{props.error}</Text>
+    else
+        return <></>
+   
+}
+
+const style = StyleSheet.create({
+    error: {
+        color: "red"
+    }
+})
--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,12 +1,13 @@
-import { PointOutput } from '@la-sectoblique/septoblique-service/dist/types/models/Point'
-
 import React, { useEffect, useState } from 'react'
-import { StyleSheet, } from 'react-native';
+import { StyleSheet, TouchableOpacity, Text} from 'react-native';
 
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { StatusBar } from 'expo-status-bar';
 
-import { getUserPoints } from '@la-sectoblique/septoblique-service';
+import { register } from '@la-sectoblique/septoblique-service';
+import { Credentials } from '@la-sectoblique/septoblique-service/dist/types/utils/Credentials';
+import { Error } from '../component/Error';
+
 
 const styles = StyleSheet.create({
     page: {
@@ -19,11 +20,46 @@ const styles = StyleSheet.create({
   });
   
 export const Register = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('')
 
+    const [message, setMessage] = useState('');
+    const [loading, setLoading] = useState(false)
+    const [error, setError] = useState('')
+
+    const handleSubmitButton = () => {
+      setLoading(true)
+      const data: Credentials = {
+        email: email,
+        password: password
+      }
+
+      //Execute register function after one second to see loading page
+      setTimeout(() => {
+        register(data)
+        .then((res) => setMessage(message))
+        .catch((err) => setError("Une erreur est survenue...Veuillez réessayer ultérieurement"))
+        .finally(() => setLoading(false))
+      }, 1000)
+      
+
+    }
+
+    if(loading)
+      //TODO: Add Proper loading page
+      return <Text>ça charge bg tkt</Text>
 
     return (
         <SafeAreaView style={styles.page}>
-        
+            {
+              //TODO: Add TextInput email and password
+            }
+            <TouchableOpacity activeOpacity={0.5} onPress={handleSubmitButton} style={{borderWidth: 1, paddingHorizontal: 5, paddingVertical: 1}}>
+              <Text>S'inscrire</Text>
+            </TouchableOpacity>
+
+            <Text>{message}</Text>
+            <Error error={error} />
             <StatusBar style="auto" />
         </SafeAreaView>
     )
