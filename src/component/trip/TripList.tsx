04c6c63a5f7782a3399fca85516517afc6e082d3
ladwein <samuel.ladwein@etu.unistra.fr>
2022-04-20 08:43:25 +0200

    feat(TripList): Pull to refresh

--- src/component/trip/TripList.tsx
+++ src/component/trip/TripList.tsx
@@ -1,21 +1,36 @@
+import { getUserTrips } from "@la-sectoblique/septoblique-service"
+import ApiError from "@la-sectoblique/septoblique-service/dist/types/errors/ApiError"
 import { TripOutput } from "@la-sectoblique/septoblique-service/dist/types/models/Trip"
-import React from "react"
-import { View, Text, FlatList, ListRenderItem } from "react-native"
+import React, { useEffect, useState } from "react"
+import { View, Text, FlatList, ListRenderItem, RefreshControl } from "react-native"
+import useTrips from "../../hook/useTrips"
 import { DebugScript } from "../utils/DebugScript"
 import { TripDetails } from "./TripDetails"
 
 interface TripListProps {
-    trips: TripOutput[],
     setActiveTrip: (arg0: TripOutput) => void
 }
 
 export const TripList = (props: TripListProps) => {
+    const [trips, initTrip, addTrip, removeTrip] = useTrips();
+    const [refreshing, setRefreshing] = useState<boolean>(true)
 
     const renderItem: ListRenderItem<TripOutput> = ({item}) => (
         <TripDetails key={item.id} trip={item} setActiveTrip={props.setActiveTrip} />
     )
+    const fetchData = () => {
+        getUserTrips()
+          .then((res: TripOutput[]) => {
+            setRefreshing(false)  
+            initTrip(res)
+        })
+        .catch((err: ApiError) => console.error(JSON.stringify(err)))
+    }
+    useEffect(() => { 
+        fetchData()      
+      }, [])
 
-    if(props.trips.length == 0)
+    if(trips.length == 0)
         return (
             <View>
                 <DebugScript />
@@ -28,9 +43,10 @@ export const TripList = (props: TripListProps) => {
         <>
         <Text style={{textAlign: "center", marginVertical: 10, fontWeight: "bold"}}>Liste des voyages</Text>
         <FlatList 
-            data={props.trips}
+            data={trips}
             renderItem={renderItem}
-            keyExtractor={(item: TripOutput, index: number) => "" + item.id}
+            keyExtractor={(item: TripOutput, index: number) => item.id.toString()}
+            refreshControl={ <RefreshControl refreshing={refreshing} onRefresh={fetchData}/>}
         />
             {/* <DebugScript /> */}
 
