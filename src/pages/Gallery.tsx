49699e9eb86a0779d282c187353cb720fe22abfd
Juliette <juliette.bressy@free.fr>
2022-04-29 17:11:59 +0200

    carousel view with flex + scroll

--- src/pages/Gallery.tsx
+++ src/pages/Gallery.tsx
@@ -1,6 +1,5 @@
-import React, { useEffect, useState, } from 'react'
-import { Button, StyleSheet, View, Image} from 'react-native';
-import * as ImagePicker from 'expo-image-picker';
+import React, { useState, } from 'react'
+import { Button, StyleSheet, Image, ScrollView} from 'react-native';
 import { uploadFile } from '@la-sectoblique/septoblique-service';
 import { MobileFileFormat } from '@la-sectoblique/septoblique-service/dist/utils/FormData';
 import { FileMetadataAttributes } from '@la-sectoblique/septoblique-service/dist/types/models/File';
@@ -9,13 +8,25 @@ import { RootTabParamList } from '../models/NavigationParamList';
 
 
 const styles = StyleSheet.create({
-  
+  carousel:{
+    flex: 1,
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    margin: 10,
+  },
+  carousel_item:{
+    width: 100,
+    height:100,
+    margin: 5,
+  }
 });
 type GalleryProps = NativeStackScreenProps<RootTabParamList, 'Gallery'>
 
   
 export const Gallery: React.FC<GalleryProps> = (props) => {
     const [image, setImage] = useState<string>();
+
+    const imageList = ["https://picsum.photos/200", "https://picsum.photos/200", "https://picsum.photos/200", "https://picsum.photos/200", "https://picsum.photos/200", "https://picsum.photos/200", "https://picsum.photos/200", "https://picsum.photos/200", "https://picsum.photos/200", "https://picsum.photos/200", "https://picsum.photos/200", "https://picsum.photos/200", "https://picsum.photos/200", "https://picsum.photos/200", "https://picsum.photos/200", "https://picsum.photos/200"];
 
     const pickImage = async () => {
       // No permissions request is necessary for launching the image library
@@ -25,22 +36,23 @@ export const Gallery: React.FC<GalleryProps> = (props) => {
         quality: 1,
       });
   
-      console.log(result);
+      console.log('result : ', result);
   
-      if (!result.cancelled) {
+      if (!result.cancelled) {        
         setImage(result.uri);
         uploadFile({extension: "png", mimeType: "image/png", id: "42", name: "couscous", tripId: 1, visibility: 'public'} as FileMetadataAttributes,{name: "test", uri: result.uri, type: "image/png"} as MobileFileFormat)
         .then((res) => console.log(res))
         .catch((err) => console.log(JSON.stringify(err)))
-
       }
 
     };
   
     return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <ScrollView>
         <Button title="Pick an image from camera roll" onPress={pickImage} />
         {image && <Image source={{ uri: image }} style={{ width: 200, height: 200 }} />}
-      </View>
+        <ImageCarousel imageList={imageList}/>
+        
+      </ScrollView>
     );
 }
