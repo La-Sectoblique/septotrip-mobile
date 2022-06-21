a9b41eac6a152264cb5dad64c191cf5fd6b367a6
ladwein <samuel.ladwein@etu.unistra.fr>
2022-06-21 20:40:40 +0200

    fix(): preview now work properly

--- src/component/trip/FileList.tsx
+++ src/component/trip/FileList.tsx
@@ -1,7 +1,6 @@
-import React, { useEffect, useState } from 'react'
-import { BackHandler, Button, Dimensions, Linking, ScrollView, Text, TouchableOpacity, View } from "react-native";
+import React, {  } from 'react'
+import { Linking, ScrollView, Text, TouchableOpacity, View } from "react-native";
 import { FileMetadataOutput } from '@la-sectoblique/septoblique-service/dist/types/models/File';
-import WebView from 'react-native-webview';
 import { deleteFile, getFileLink } from '@la-sectoblique/septoblique-service';
 import Toast from "react-native-toast-message"
 import { AntDesign, Entypo, FontAwesome5, MaterialCommunityIcons } from '@expo/vector-icons';
@@ -13,15 +12,10 @@ interface FileListProps {
 }
 
 export const FileList = ({files, showWebView, refresh}: FileListProps) => {
-    const [fileURL, setFileURL] = useState<string>("");
-    const [selectedFile, setSelectedFile] = useState<FileMetadataOutput>({} as FileMetadataOutput)
 
     const downloadFile = async (fileMetaData: FileMetadataOutput) => {
         if(showWebView == false) return 
         const url = await getFileLink(fileMetaData.tripId, fileMetaData.id)
-        setFileURL(url)
-        setSelectedFile(fileMetaData) 
-
         const supported = await Linking.canOpenURL(url);
 
         if (supported) {
@@ -37,46 +31,12 @@ export const FileList = ({files, showWebView, refresh}: FileListProps) => {
         }
     }
 
-    useEffect(() => {
-        const onBackPress = () => {
-            if(fileURL !== ""){
-                setFileURL("")
-                return true
-            }else{
-                return false
-            }
-                
-        }
-        BackHandler.addEventListener('hardwareBackPress', onBackPress);
-    })
-
-
     const onPress = ((file: FileMetadataOutput) => {
         deleteFile(file.tripId, file.id)
         .then(() => {
             refresh()
         })
     })
-
-    if(fileURL !== "" && showWebView != false && selectedFile.extension !== "pdf")
-        return (
-            <View style={{flex: 1, margin: 5}}>
-                 
-                <WebView
-                    source={{ uri: fileURL}}
-                    style={{ flex: 1, width: Dimensions.get('window').width }}
-                    allowFileAccess={true}
-                    javaScriptEnabled={true}
-                    allowUniversalAccessFromFileURLs={true}
-                    originWhitelist={["*"]}
-                />
-            
-                <Button 
-                title="Fermer"
-                onPress={() => setFileURL("")}
-                />
-            </View>
-        )
 
     return (
         <ScrollView style={{borderWidth: 1, margin: 5}}>
